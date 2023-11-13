21776aee331edd1e755e799312904ab03ca87b19
juanj <juanjosegr9@gmail.com>
2023-11-13 14:52:30 +0100

    Arreglo del build.gradle.kts y creación de fun principal

--- app/src/main/java/com/example/myapplication/Screens/funciones.kt
+++ app/src/main/java/com/example/myapplication/Screens/funciones.kt
@@ -1,8 +1,15 @@
 package com.example.myapplication.Screens
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 
 @Composable
-fun Inicio(){
+fun Inicio() {
+
+    var textoSaludar by rememberSaveable { mutableStateOf("") }
+
 
 }
